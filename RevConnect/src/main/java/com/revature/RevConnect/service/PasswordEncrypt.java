d9a3bac08c70ed04634ae792b96d9bae31103df1
Alden <39203099+Warmedpie@users.noreply.github.com>
2024-08-01 12:41:51 -0400

    Slightly better encryption

--- RevConnect/src/main/java/com/revature/RevConnect/service/PasswordEncrypt.java
+++ RevConnect/src/main/java/com/revature/RevConnect/service/PasswordEncrypt.java
@@ -1,14 +1,20 @@
 package com.revature.RevConnect.service;
 
+import java.util.Random;
+
 public class PasswordEncrypt {
 
-    //This encrypt algorithm will have a lot of collision, but for the project this will be fine
+    //This encryption algorithm will have a lot of collision, but for the project this will be fine
     public static String encrypt(String password) {
+
+        int seed = (int)password.charAt(0);
+        Random generator = new Random(seed);
+        int RNG = generator.nextInt();
 
         StringBuilder encryptPassword = new StringBuilder();
 
         for (int i = 0; i < password.length(); i++) {
-            encryptPassword.append(((password.charAt(i)) % 26) + (int)'A');
+            encryptPassword.append(((password.charAt(i)) % RNG));
         }
 
         return encryptPassword.toString();
